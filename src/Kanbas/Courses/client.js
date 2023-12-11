a12c484e02f58bc4f0a62255de52f380272245b7
Yimeng Chen <yimeng2002@icloud.com>
2023-12-11 13:36:12 -0500

    change url

--- src/Kanbas/Courses/client.js
+++ src/Kanbas/Courses/client.js
@@ -1,38 +1,30 @@
 import axios from "axios";
+const COURSES_URL =
+  "https://kanbas-node-server-app-i1az.onrender.com/api/courses";
 
 export const fetchCourses = async () => {
-  const response = await axios.get("http://localhost:4000/api/courses");
+  const response = await axios.get(COURSES_URL);
   return response.data;
 };
 
 export const fetchCourse = async (id) => {
-  const response = await axios.get(`http://localhost:4000/api/courses/${id}`);
+  const response = await axios.get(`COURSES_URL/${id}`);
   return response.data;
 };
 
 export const deleteCourse = async (id) => {
-  const response = await axios.delete(
-    `http://localhost:4000/api/courses/${id}`
-  );
+  const response = await axios.delete(`COURSES_URL/${id}`);
   return response.data;
 };
 
 export const updateCourse = async (course) => {
-  const response = await axios.put(
-    `http://localhost:4000/api/courses/${course._id}`,
-    course
-  );
+  const response = await axios.put(`COURSES_URL/${course._id}`, course);
   console.log(response.data._id);
   return response.data;
 };
 
-
-
 export const addCourse = async (course) => {
-  const response = await axios.post(
-    "http://localhost:4000/api/courses",
-    course
-  );
+  const response = await axios.post("COURSES_URL", course);
   console.log("add course: " + response.data._id);
   return response.data;
 };
